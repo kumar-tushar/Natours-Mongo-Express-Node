843f1bcb4c8e88b497b3ec5325bf785dfb9deb97
kumar-tushar <tushar.workplace@gmail.com>
2023-05-01 21:51:15 +0530

    new tour creation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,71 +1,72 @@
-const fs = require("fs")
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, "utf-8"))
+const Tour = require("../models/tourModel")
 
 // Route Handler - Get all Tours
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   })
 }
 
 // Route Handler - Get one Tour
 exports.getTour = (req, res) => {
   const id = Number(req.params.id)
-  const tour = tours.find((tour) => tour.id === id)
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
+  // const tour = tours.find((tour) => tour.id === id)
+  //
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
 
   res.status(200).json({
     status: "success",
-    data: { tour },
+    // data: { tour },
   })
 }
 
 // Route Handler - Create a new Tour
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
-  tours.push(newTour)
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body)
 
-  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), () => {
     res.status(201).json({
       status: "success",
-      data: { tour: newTour },
+      data: { newTour },
     })
-  })
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    })
+  }
 }
 
 // Route Handler - Update a Tour
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id )>= tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
+  // if (Number(req.params.id )>= tours.length) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
 
   res.status(200).json({
     status: "success",
-    data: { tour: "Updated Tour" },
+    // data: { tour: "Updated Tour" },
   })
 }
 
 // Route Handler - Delete a Tour
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id >= tours.length)) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
+  // if (Number(req.params.id >= tours.length)) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
 
   res.status(204).json({
     status: "success",
