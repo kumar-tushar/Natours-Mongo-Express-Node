11656d5e33890187109fd7ba351f05adef6a6ce0
kumar-tushar <tushar.workplace@gmail.com>
2023-05-04 04:28:03 +0530

    sorting done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,7 +11,16 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (e) => `$${e}`)
 
-    const query = Tour.find(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr))
+
+    // Sorting Query
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ")
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort("-createdAt")
+    }
+
     const tours = await query
 
     // Sending Response
