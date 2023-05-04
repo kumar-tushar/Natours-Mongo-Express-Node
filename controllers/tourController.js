5d3acb2da6e3ec4629eedbc2543474201315c52c
kumar-tushar <tushar.workplace@gmail.com>
2023-05-04 09:43:58 +0530

    pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select("-__v")
     }
 
+    // Pagination Query
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 100
+    const skip = (page - 1) * limit
+
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()
+      if (skip >= numTours) throw new Error("This page doesn't exist")
+    }
+
+    // Query Execution
     const tours = await query
 
     // Sending Response
