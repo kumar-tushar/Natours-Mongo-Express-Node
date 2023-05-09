1198a0b36a8e97110e43697d78a024663b779480
kumar-tushar <tushar.workplace@gmail.com>
2023-05-09 11:21:46 +0530

    global error handlers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
@@ -7,7 +9,15 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+// Middleware - Undefined Routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+app.use(globalErrorHandler)
 
 module.exports = app
